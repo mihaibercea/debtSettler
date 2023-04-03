c6a74a89de3369d9ca5141d7eda05e3087bf959f
Mișu Bercea <mihai.bercea@gmail.com>
2023-04-03 16:50:36 +0300

    prod

--- debtSettler/settings.py
+++ debtSettler/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8s)dem!%f8shurs6wz)gvqnu03d=lmqnj4pf492(%z&v=mqm1l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
