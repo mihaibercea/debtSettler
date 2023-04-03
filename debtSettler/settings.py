8e26ec135187507fa15529e80e6c1df6d0b8a154
Mișu Bercea <mihai.bercea@gmail.com>
2023-04-03 16:51:28 +0300

    Update settings.py

--- debtSettler/settings.py
+++ debtSettler/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'django-insecure-8s)dem!%f8shurs6wz)gvqnu03d=lmqnj4pf492(%z&v=mqm1l
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#TEST2
 
 ALLOWED_HOSTS = []
 
